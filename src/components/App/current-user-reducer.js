8c986fc1be1f31b00e7926b6d71525775b56c9c7
Anna Brikodili <brikodili.troyan@gmail.com>
2019-09-15 10:56:56 +0300

    Update

--- src/components/App/current-user-reducer.js
+++ src/components/App/current-user-reducer.js
@@ -1,11 +1,7 @@
 export default function(state, {type, payload}) {
-  const ROLE_MAPPINGS = {
-    provider: 1,
-    customer: 2,
-  };
   switch (type) {
     case 'SET_CURRENT_USER':
-      return {...payload, ...{role: ROLE_MAPPINGS[payload.role]}};
+      return {...payload};
     case 'CLEAR_CURRENT_USER':
       return undefined;
     default:
